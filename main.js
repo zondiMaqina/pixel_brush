97b91fbfce6960d88934dd62c673c4d9c6552f13
zondiMaqina <zondimaqina0@gmail.com>
2025-06-06 20:28:52 +0200

    add event listener for theme icons

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ const allColors = {
   yellow: 'yellow'
 };
 
-// function for showing pop up model and removing it
+// for showing pop up model and removing it
 const settingsImage = document.querySelector("ul.right li img#settings-button");
 const model = document.querySelector("header div.settings-container");
 
 settingsImage.addEventListener("click", () => {
   model.classList.toggle("show");
+})
+
+// for changing theme of webpage
+const body = document.querySelector("body");
+const featuresPane = document.querySelector("article.features-pane");
+const sunIcon = document.querySelector("nav ul li img#theme-button");
+
+sunIcon.addEventListener("click", () => {
+  body.classList.toggle('light');
+  featuresPane.classList.toggle('light');
 })
