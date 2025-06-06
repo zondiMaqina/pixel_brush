60318a22c30841622206f7cedc4edaf3ae810d9e
zondiMaqina <zondimaqina0@gmail.com>
2025-06-06 20:01:14 +0200

    add event listener to settings img

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const allColors = {
   orange: 'orange',
   yellow: 'yellow'
 };
+
+// function for showing pop up model and removing it
+const settingsImage = document.querySelector("ul.right li img#settings-button");
+const model = document.querySelector("header div.settings-container");
+
+settingsImage.addEventListener("click", () => {
+  model.classList.toggle("show");
+})
