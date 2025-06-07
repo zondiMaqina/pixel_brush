ada19d75b02c6d9f22efe4fecf85f5e15732f04e
zondiMaqina <zondimaqina0@gmail.com>
2025-06-07 14:37:46 +0200

    add grid layout filtering feature

--- main.js
+++ main.js
@@ -30,6 +30,16 @@ const settingsModel = document.querySelector("div.settings-container article#set
 const increment = document.querySelector("div.settings-container article p.increment");
 const decrement = document.querySelector("div.settings-container article p.decrement");
 const input = document.querySelector("div.settings-container article#settings input");
+let sketchPad = document.querySelector("main section#sketch-pad");
+
+function addBlocksToSketchPad(number) {
+  sketchPad.replaceChildren(document.createElement("div"));
+  let square = number ** 2;
+  for (let num = 1; num < square; num++) {
+    sketchPad.appendChild(document.createElement('div'));
+  }
+  sketchPad.setAttribute("style", `grid-template-columns: repeat(${number}, 1fr); grid-template-rows: repeat(${number}, 1fr);`)
+}
 
 settingsModel.addEventListener('click', (event) => {
   let target = event.target;
@@ -38,8 +48,10 @@ settingsModel.addEventListener('click', (event) => {
   if (target == increment && value < 16){
     value += 1;
     input.value = value;
+    addBlocksToSketchPad(value)
   } else if (target == decrement && value > 2){
     value -= 1;
     input.value = value;
+    addBlocksToSketchPad(value)
   }
 });
